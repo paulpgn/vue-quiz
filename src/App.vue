8936d0e7a3307b4bb3aff45591ffb21a340b501e
paulpgn <105318234+paulpgn@users.noreply.github.com>
2022-05-12 22:13:16 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<script setup>
+import {ref, computed} from 'vue'
+
+const questions = ref([
+  {
+    question: "When was the first man on the moon ?",
+    answer: 0,
+    options: [
+      "1969",
+      "1968",
+      "1967"
+    ],
+    selected: null
+  },
+  {
+  question: "What is two zero two one ?",
+  answer: 2,
+  options: [
+    "2011",
+    "0021",
+    "2121",
+    "0011"
+  ],
+  selected: null
+  },
+])
+</script>
+
+<template>
+<h1>Hello World</h1>
+</template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Montserrat', sans-serif;
+}
+body {
+  background-color: #271C36;
+  color: #FFF;
+}
+</style>
